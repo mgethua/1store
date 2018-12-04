8b0d17c6faff9ff8c6632973f05dbb19ae6e207b
Linweibin <719452518@qq.com>
2018-12-04 22:21:01 +0800

    修改了登录界面的样式,添加了注册页和登录页的点击事件

---
+++ app/static/js/login_click.js
@@ -0,0 +1,22 @@
+var $two_code = document.querySelector('.two_code');
+var $codeBox = document.querySelector('.codeBox');
+var $pc = document.querySelector('.pc');
+var $form_footer =document.querySelector('.form_footer');
+var $icon =document.querySelector('.icon');
+var $iconAll =document.querySelector('.iconAll');
+var $main =document.querySelector('.main');
+$two_code.onclick=function(){
+    $codeBox.style.zIndex="1"
+}
+$pc.onclick=function(){
+    $codeBox.style.zIndex="-1"
+}
+$icon.onclick=function(){
+    if($iconAll.style.display =='block'){
+        $iconAll.style.display ='none';
+        $main.style.height= '440px'
+    } else{
+        $iconAll.style.display ='block';
+        $main.style.height= '520px'
+    } 
+}
